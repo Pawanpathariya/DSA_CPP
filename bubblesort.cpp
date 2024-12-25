cbf8462184752cba2fb83cadbad34f8409ad3e0e
Pawanpathariya <pawanpathariys@gmail.com>
2024-12-25 07:31:12 +0530

    bubble sort

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,12 +1,23 @@
 #include<iostream>
 using namespace std;
-
 int bubble(int arr[],int l){
 for(int i=0;i<l;i++){
-    cout<<arr[i]<<" ";
+    bool f=true;
+    for(int j=0;j<l-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+            f=false;
+        }
+    }
+    if (f){
+        break;
+    }
 }
 }
 int main(){
+    //driver code
     int r;
     cout<<"Enter length of arr ";
     cin>>r;
@@ -16,10 +27,15 @@ int main(){
         for(int i=0;i<r;i++){
             cin>>arr[i];
         }
+cout<<"Before sorting"<<endl;
+        for(int i=0;i<r;i++){
+    cout<<arr[i]<<" ";}
+            bubble(arr,r);
+    cout<<endl<<"After sorting"<<endl;
+             for(int i=0;i<r;i++){
+    cout<<arr[i]<<" ";}
     }
     else{
         cout<<"Invalid input";
-    }
-
-    bubble(arr,r);
+    } 
 }
