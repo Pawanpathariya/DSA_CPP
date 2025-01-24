4bcb75b27603ad0418a8b125d327d23ef28aa7a8
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-24 12:17:04 +0530

    namespace template

--- namespace.cpp
+++ namespace.cpp
@@ -12,7 +12,18 @@ namespace cybrom{
     //     cout<<"\ngood()";
     // }
 
+template<typename cybrom>
+class course{
+    cybrom name;
+  public:
+   void input(cybrom n){
+      name=n;
+  }
 
+cybrom show(){
+    return name;
+}
+};
     class bplstream{
    public:
    void bhopal(){
@@ -25,6 +36,11 @@ using namespace cybrom;
 int main(){
 // show();
 // good();
+
+course<string> obj;
+obj.input("Pawan");
+cout<<obj.show();
+
 
 bplcout.bhopal();
 }
