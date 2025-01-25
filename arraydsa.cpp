03ef23204bdd4757afc29cc095999fa19b194932
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-25 12:17:52 +0530

    array of dsa

--- arraydsa.cpp
+++ arraydsa.cpp
@@ -4,14 +4,19 @@ using namespace std;
 int main(){
 
     array<int ,6>b{11,2,3,1,5,6};
-    cout<<"size="<<b.size()<<" \n";
-    for(int i=0;i<b.size();i++){
-        cout<<b[i]<<"\t";
-    }
+//     cout<<"size="<<b.size()<<" \n";
+//     for(int i=0;i<b.size();i++){
+//         cout<<b[i]<<"\t";
+//     }
 
-cout<<endl;
-    for(int j=0;j<9;j++){
-        cout<<b.at(j)<<"\t";
-    }
+// cout<<endl;
+//     for(int j=0;j<9;j++){
+//         cout<<b.at(j)<<"\t";
+//     }
+
+for(auto p:b){
+    cout<<p<<"\t";
+}
+
 
 }
