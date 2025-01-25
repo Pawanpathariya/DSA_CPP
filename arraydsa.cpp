9a2648d8f94495a4df192f3c2ce7fab815700ca8
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-25 12:15:18 +0530

    array of dsa

---
+++ arraydsa.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+#include <array>
+int main(){
+
+    array<int ,6>b{11,2,3,1,5,6};
+    cout<<"size="<<b.size()<<" \n";
+    for(int i=0;i<b.size();i++){
+        cout<<b[i]<<"\t";
+    }
+
+cout<<endl;
+    for(int j=0;j<9;j++){
+        cout<<b.at(j)<<"\t";
+    }
+
+}
