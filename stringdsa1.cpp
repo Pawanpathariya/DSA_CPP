4f0b310fd34e751b7984a810e752a46dac1ad8b2
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-29 12:30:12 +0530

    str

--- stringdsa1.cpp
+++ stringdsa1.cpp
@@ -30,19 +30,28 @@ cout<<"Name is : "<<name;
 
 #include<iostream>
 using namespace std;
+#include<algorithm>
 #include<string>
 
 int main(){
-string name="Pawan";
+// string name="Pawan";
 
-cout<<"\nName is : "<<name;
+// cout<<"\nName is : "<<name;
 
-name.push_back('a');
-cout<<"\nName is : "<<name;
+// name.push_back('a');
+// cout<<"\nName is : "<<name;
 
-name.pop_back();
-cout<<"\nName is : "<<name;
-name.pop_back();
-cout<<"\nName is : "<<name;
+// name.pop_back();
+// cout<<"\nName is : "<<name;
 
+
+
+// string name="Pawan";
+// for(auto l:name){
+//     cout<<l<<"\t";
+// }
+
+string name="pawan";
+sort(name.begin(),name.end());
+cout<<"\nName is : "<<name;
 }
