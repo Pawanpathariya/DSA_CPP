2766504d0ef079d7c7f5c333e1ace6381d3298e1
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-29 12:10:37 +0530

    str

--- stringdsa1.cpp
+++ stringdsa1.cpp
@@ -1,13 +1,27 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cout<<"Enter size of string \n";
+// cin>>n;
+// cin.ignore();
+// char s[n];
+// cout<<"Enter string \n";
+// cin.getline(s,n);
+// cout<<"String is \n";
+// cout<<s;
+// }
+
+
+
 #include<iostream>
 using namespace std;
+#include<string>
+
 int main(){
-int n;
-cout<<"Enter size of string \n";
-cin>>n;
-cin.ignore();
-char s[n];
-cout<<"Enter string \n";
-cin.getline(s,n);
-cout<<"String is \n";
-cout<<s;
+string name;
+cout<<"Enter name \n";
+getline(cin,name);
+cout<<"Name is : "<<name;
+
 }
