511a589cfc860618469135ec73e6dc5b11df1dfb
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-29 12:18:35 +0530

    str

--- stringdsa1.cpp
+++ stringdsa1.cpp
@@ -15,7 +15,7 @@
 
 
 //for string
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 #include<string>
 
@@ -24,5 +24,25 @@ string name;
 cout<<"Enter name \n";
 getline(cin,name);
 cout<<"Name is : "<<name;
+
+}*/
+
+
+#include<iostream>
+using namespace std;
+#include<string>
+
+int main(){
+string name="Pawan";
+
+cout<<"\nName is : "<<name;
+
+name.push_back('a');
+cout<<"\nName is : "<<name;
+
+name.pop_back();
+cout<<"\nName is : "<<name;
+name.pop_back();
+cout<<"\nName is : "<<name;
 
 }
