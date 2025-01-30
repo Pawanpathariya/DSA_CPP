319f26a32eb71cf5ba6f83225a9c40a99c3d3e2d
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-30 12:07:35 +0530

    vector

---
+++ vector.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+vector<int>v;
+v.push_back(1);
+cout<<"Size "<<v.size()<<endl;
+cout<<"Capacity "<<v.capacity()<<endl;
+v.push_back(2);
+cout<<"Size "<<v.size()<<endl;
+cout<<"Capacity "<<v.capacity()<<endl;
+v.push_back(3);
+cout<<"Size "<<v.size()<<endl;
+cout<<"Capacity "<<v.capacity()<<endl;
+v.push_back(4);
+cout<<"Size "<<v.size()<<endl;
+cout<<"Capacity "<<v.capacity()<<endl;
+v.push_back(5);
+cout<<"Size "<<v.size()<<endl;
+cout<<"Capacity "<<v.capacity()<<endl;
+v.push_back(7);
+cout<<"Size "<<v.size()<<endl;
+cout<<"Capacity "<<v.capacity()<<endl;
+v.push_back(8);
+cout<<"Size "<<v.size()<<endl;
+cout<<"Capacity "<<v.capacity()<<endl;
+
+
+
+}
