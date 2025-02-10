82836bca11ef408c63ffd213ceb9945d4a408f56
Pawanpathariya <pawanpathariys@gmail.com>
2025-02-10 12:18:31 +0530

    lemda expression in singularlist

--- singlink.cpp
+++ singlink.cpp
@@ -1,23 +1,43 @@
+// #include <iostream>
+// using namespace std;
+// #include<forward_list>
+// int main(){
+
+//     forward_list<int>f1{3,5,6,4,5};
+//     forward_list<int>f2{13,15,16,14,15};
+
+//     for(auto p:f1){
+//         cout<<p<<" ";
+//     }
+//     cout<<endl;
+//     for(auto p:f2){
+//         cout<<p<<" ";
+//     }
+// cout<<"\nAfter merging"<<endl;
+// f1.sort();
+// f2.sort();
+// f1.merge(f2);
+// for(auto p:f1){
+//     cout<<p<<" ";
+// }
+// }
+
+
+//lemda function :- 
+
 #include <iostream>
 using namespace std;
 #include<forward_list>
 int main(){
 
-    forward_list<int>f1{3,5,6,4,5};
-    forward_list<int>f2{13,15,16,14,15};
+    forward_list<int>f1{3,5,6,4,10,15,16,14};
 
-    for(auto p:f1){
-        cout<<p<<" ";
+    f1.remove_if([](int n) { return n > 5; });
+
+    for(auto p : f1){
+        cout << p << " ";
     }
-    cout<<endl;
-    for(auto p:f2){
-        cout<<p<<" ";
-    }
-cout<<"\nAfter merging"<<endl;
-f1.sort();
-f2.sort();
-f1.merge(f2);
-for(auto p:f1){
-    cout<<p<<" ";
-}
+
+
+
 }
