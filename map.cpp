b5b58f7cf3830d26fe74fb75393c7653f22d36ec
Pawanpathariya <pawanpathariys@gmail.com>
2025-02-13 12:02:09 +0530

    map

---
+++ map.cpp
@@ -0,0 +1,11 @@
+//1)it is a associative container , 2)it uses pair value that is key,value ,3)it display the data in sorted order 
+//4)it uses self balance binary search tree concept 5)key is always unique but value can be duplicate.
+//it has 3 type 1)Ordered map 2)Unordered map 3)Multimap
+
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+    map<int,string> m;
+    
+}
