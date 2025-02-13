617dbf1ba96e1b011285c83443898d2236149d62
Pawanpathariya <pawanpathariys@gmail.com>
2025-02-13 12:25:25 +0530

    map

--- map.cpp
+++ map.cpp
@@ -7,9 +7,25 @@
 using namespace std;
 int main(){
     map<int,string> m;
+    /* first step to insert data in map
     m.insert({2,"Sumit"});
     m.insert({1,"Pawan"});
     m.insert({3,"Rahul"});
+    */
+/*
+second step to insert data in map
+    m[2]="Sumit";
+    m[1]="Pawan";
+    m[3]="Rahul";
+    */
+
+/*
+//third step to insert data in map in string it give null value and in int it give 0 value if we didn't insert any value
+m[2]="Sumit";
+m[1];*/
+
+
+m={{1,"pawan"},{2,"sumit"},{3,"rahul"}};
 for(auto p:m){
     cout<<p.first<<" "<<p.second<<"\n";
 }
