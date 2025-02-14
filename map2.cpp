c182e3b9fad75055106fdae759899e450bea3394
Pawanpathariya <pawanpathariys@gmail.com>
2025-02-14 12:13:00 +0530

    find in map

--- map2.cpp
+++ map2.cpp
@@ -21,5 +21,15 @@ cout<<"result \n";
 for(auto p:m){
     cout<<p.first<<" "<<p.second<<endl;
 }
+//searching code
+string s;
+cout<<"Enter name to search ";
+cin>>s;
+
+if(m.find(s)!=m.end()){
+  cout<<m[s];
+}
+else{
+    cout<<"not found";  }
 
 }
