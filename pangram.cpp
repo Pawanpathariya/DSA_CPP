2ded53860f436d14c0c1d8b63731b0f65d38b68b
Pawanpathariya <pawanpathariys@gmail.com>
2025-02-18 12:26:10 +0530

    pangram

---
+++ pangram.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+bool isPangram(string& str) {
+    unordered_set<char> alphabet;
+    for (const char& c : str) {
+        if (isalpha(c)) { 
+            alphabet.insert(tolower(c)); 
+        }
+    }
+    return alphabet.size() == 26;
+}
+
+int main() {
+    string input;
+    cout << "Enter a sentence: ";
+    getline(cin, input);
+
+    if (isPangram(input)) {
+        cout << "The sentence is a pangram.\n";
+    } else {
+        cout << "The sentence is not a pangram.\n";
+    }
+
+    return 0;
+}
