c4cf0ed52863a7633431786f7d92abaa98ce5953
Pawanpathariya <pawanpathariys@gmail.com>
2025-02-28 14:14:04 +0530

    valid paranthesis

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,5 +1,1 @@
-v.erase(it-1);
-// cout<<"\nResult after delete : \n";
-// for(auto p:v){
-//     cout<<p<<" ";
-// }
+#include <algorithm>
--- validparenthesis.cpp
+++ validparenthesis.cpp
@@ -1,60 +1,24 @@
+// 
 #include<iostream>
-using namespace std;
 #include<stack>
-bool valid(string s){
-    bool f=true;
-stack<char>st;
-for(int i=0;i<s.length();i++){
-    if(s[i]=='[' || s[i]=='{' || s[i]=='('){
-        st.push(s[i]);
-    }
-    else if(s[i]==']'){
-if(!st.empty() && st.top()=='['){
-st.pop();
-}
+using namespace std;
 
-else{
-    f=false;
-    break;
-}
-}
-else if(s[i]=='}'){
-    if(!st.empty() && st.top()=='{'){
-    st.pop();
-    }
-    
-    else{
-        f=false;
-        break;
-    }
-    }
-
-    else if(s[i]==')'){
-        if(!st.empty() && st.top()=='('){
-        st.pop();
+bool valid(string s) {
+    stack<char> st;
+    for (char c : s) {
+        if (c == '[' || c == '{' || c == '(') {
+            st.push(c);
+        } else {
+            if (st.empty() || (c == ']' && st.top() != '[') || (c == '}' && st.top() != '{') || (c == ')' && st.top() != '(')) {
+                return false;
+            }
+            st.pop();
         }
-        
-        else{
-            f=false;
-            break;
-        }
-        }
-
-    }
-
-    if(!st.empty()){
-        return false;
-    }
-}
-int main(){
-
-string s="[{]";
-if(valid(s)){
-cout<<"true";
-}
-else{
-    cout<<"false";
+    } 
+    return st.empty();
 }
 
-
+int main() {
+    string s = "[{}]";
+    cout << (valid(s) ? "true" : "false");
 }
